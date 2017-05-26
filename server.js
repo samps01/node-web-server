dc4440d440621c8a96d22400172256093901e64e
samps01 <samps01@gmail.com>
2017-05-25 20:56:45 -0400

    New features added

--- server.js
+++ server.js
@@ -46,6 +46,13 @@ app.get('/about',(req,res)=>{
     });
 })
 
+app.get('/projects',(req,res)=>{
+    res.render('projects.hbs',{
+        pageTitle:'Projects',
+        pageDescription:'List of all Projects'
+    })
+});
+
 app.get('/bad',(req,res)=>{
     res.send({
         errorMessage: 'Bad request'
